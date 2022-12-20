6e54e9bb4d39d91b17e2369fe2eac7289f12af05
Nicole Saunders <nicostar26@yahoo.com>
2022-12-20 08:16:05 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <div class="wrapper">
+      <video margin="0" autoplay loop>
+        <source src="./assets/header-video.mp4" type="video/mp4">
+      </video> 
+
+      <Footer />
+  </div>
+
+  
+</template>
+
+<style scoped>
+@import './style.css';
+.wrapper {
+  padding: 0px;
+  margin: 0px;
+}
+video {
+  width: 100%;
+  height: 100%;
+  border-bottom: .1rem #ac8c3c solid;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import Style from '../src/style.css';
+import App from './App.vue'
+
+createApp(App).mount('#app')
